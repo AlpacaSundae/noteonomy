6751dac8ab004953956bb54037730ff3dc5152cc
AlpacaSundae <jaicobschott@hotmail.com>
2024-04-10 05:00:56 +0800

    allow only alphanumerical paths

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::Path, routing::get, Router
+    extract::Path, routing::get, Router,
 };
 
 use std::fs;
@@ -20,17 +20,26 @@ async fn main() {
 // Takes in a path to a note within the notes folder
 // Returns the contents or provides an error string if it fails
 async fn get_note_contents(Path(name): Path<String>) -> String {
-    fs::read_to_string(format!("notes/{}", name)).unwrap_or_else( |err| {
-        if err.kind() == std::io::ErrorKind::NotFound {
-            format!("the note '{}' does not exist!", name)
-        } else {
-            println!("{err}");
-            error_page()
-        }
-    })
+    if _clean_path(&name) {
+        fs::read_to_string(format!("notes/{}", name)).unwrap_or_else( |err| {
+            if err.kind() == std::io::ErrorKind::NotFound {
+                format!("the note '{}' does not exist!\nAvailable notes:\nTODO", name)
+            } else {
+                println!("{err}");
+                error_page()
+            }
+        })
+    } else {
+        format!("the path '{}' is invalid. We only accept alphanumericals in these paths", name)
+    }
 }
 
-async fn set_note_contents() {}
+async fn set_note_contents() {
+    // if file isnot exist, create it
+}
+
+
+
 async fn delete_note() {}
 
 async fn get_note_list() -> String {
@@ -39,4 +48,33 @@ async fn get_note_list() -> String {
 
 fn error_page() -> String {
     String::from("ERROR ERROR ERROR ERROR ERROR ERROR ")
+}
+
+// Ensure a path has no funky characters in it before trying to access a file
+// for now, we only accept alphanumeric paths 
+fn _clean_path(name : &String) -> bool {
+    for c in name.chars() {
+        match c {
+            'a'..='z'   => (),
+            'A'..='Z'   => (),
+            '0'..='9'   => (),
+            '/'         => (),
+            _           => return false
+        }
+    }
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::_clean_path;
+
+    #[test]
+    fn test_clean_path() {
+        assert!(  _clean_path(&String::from("this/is/67/aGOODpath")));
+        assert!(! _clean_path(&String::from("this/IS/67/a<>badpath")));
+
+        assert!(  _clean_path(&String::from("")));
+        assert!(! _clean_path(&String::from(" ")));
+    }
 }
